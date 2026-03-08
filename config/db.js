218a069a204bb41e00b2cd73e446460cdf8e3303
Gaurav <gauravprajapati1903@gmail.com>
2026-03-08 13:23:34 +0530

    backend initial commit

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
 const { Sequelize } = require("sequelize");
 const { secret } = require("./secret");
-const sequelize = new Sequelize(
-  process.env.db_name,
-  process.env.db_user,
-  process.env.db_pass,
-  {
-    host: process.env.db_host || "localhost",
-    dialect: "mysql",
-    logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
-);
+});
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅  MySQL connected");
+    console.log("✅  PostgreSQL connected");
   } catch (err) {
     console.error("❌  DB connection failed:", err.message);
   }
